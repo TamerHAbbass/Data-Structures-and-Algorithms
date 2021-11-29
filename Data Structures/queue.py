420e33df92c3f3019df9d8772659988ad65e0915
Tamer Abbass <you@example.com>
2021-11-29 09:20:23 -0800

    Added all queue implementation samples

--- Data Structures/queue.py
+++ Data Structures/queue.py
@@ -22,3 +22,65 @@ class Queue:
 
     def size(self):
         return len(self.items)
+
+
+"""
+Python Queue module implementation.
+"""
+from queue import Queue
+
+q = Queue(maxsize = 3)
+ 
+# Add items to a queue
+q.put('a')
+ 
+# Remove items from queue
+q.get()
+
+
+"""
+Deque implementation of a queue.
+"""
+from collections import deque
+
+customers = deque()
+
+# Add items to a queue
+customers.append("Jane")
+
+# Remove items from a queue
+customers.popleft()
+
+
+"""
+Alternate Deque implementation.
+"""
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self._items = deque()
+
+    def enqueue(self, item):
+        self._items.append(item)
+
+    def dequeue(self):
+        try:
+            return self._items.popleft()
+        except IndexError:
+            raise IndexError("dequeue from an empty queue") from None
+
+    def __len__(self):
+        return len(self._items)
+
+    def __contains__(self, item):
+        return item in self._items
+
+    def __iter__(self):
+        yield from self._items
+
+    def __reversed__(self):
+        yield from reversed(self._items)
+
+    def __repr__(self):
+        return f"Queue({list(self._items)})"
